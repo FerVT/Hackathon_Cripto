9aecfab8008b79b016ab4c67951091e8a23af008
Fer <fer.vargas.torres@gmail.com>
2018-04-03 21:15:15 +0000

    Compilacion 1

---
+++ builds/schmeckles-all-files/src/CMakeFiles/CryptoNoteCore.dir/DependInfo.cmake
@@ -0,0 +1,84 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/Account.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Account.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/AddBlockErrorCondition.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/AddBlockErrorCondition.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/AddBlockErrors.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/AddBlockErrors.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/BlockValidationErrors.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/BlockValidationErrors.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/BlockchainCache.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/BlockchainCache.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/BlockchainMessages.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/BlockchainMessages.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/BlockchainReadBatch.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/BlockchainReadBatch.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/BlockchainStorage.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/BlockchainStorage.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/BlockchainUtils.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/BlockchainUtils.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/BlockchainWriteBatch.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/BlockchainWriteBatch.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/CachedBlock.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CachedBlock.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/CachedTransaction.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CachedTransaction.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/Checkpoints.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Checkpoints.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/Core.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Core.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/CoreErrors.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CoreErrors.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/CryptoNoteBasic.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteBasic.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/CryptoNoteBasicImpl.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteBasicImpl.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/CryptoNoteFormatUtils.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteFormatUtils.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/CryptoNoteSerialization.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteSerialization.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/CryptoNoteTools.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteTools.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/Currency.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Currency.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/DBUtils.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/DBUtils.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/DataBaseConfig.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/DataBaseConfig.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/DataBaseErrors.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/DataBaseErrors.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/DatabaseBlockchainCache.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/DatabaseBlockchainCache.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/DatabaseBlockchainCacheFactory.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/DatabaseBlockchainCacheFactory.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/DatabaseCacheData.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/DatabaseCacheData.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/Difficulty.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Difficulty.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/ITimeProvider.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/ITimeProvider.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/MainChainStorage.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/MainChainStorage.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/MemoryBlockchainCacheFactory.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/MemoryBlockchainCacheFactory.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/MemoryBlockchainStorage.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/MemoryBlockchainStorage.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/Miner.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Miner.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/MinerConfig.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/MinerConfig.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/RocksDBWrapper.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/RocksDBWrapper.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/SwappedBlockchainStorage.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/SwappedBlockchainStorage.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/SwappedMap.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/SwappedMap.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/SwappedVector.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/SwappedVector.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/Transaction.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Transaction.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/TransactionExtra.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionExtra.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/TransactionPool.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionPool.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/TransactionPoolCleaner.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionPoolCleaner.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/TransactionPoolMessages.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionPoolMessages.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/TransactionPrefixImpl.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionPrefixImpl.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/TransactionUtils.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionUtils.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/TransactionValidationErrors.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionValidationErrors.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/TransactionValidatiorState.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionValidatiorState.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/UpgradeDetector.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/UpgradeDetector.cpp.o"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src/CryptoNoteCore/UpgradeManager.cpp" "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/UpgradeManager.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  "../../src/Platform/Posix"
+  "../../external/rocksdb/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/Common.dir/DependInfo.cmake"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/Crypto.dir/DependInfo.cmake"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/Logging.dir/DependInfo.cmake"
+  "/home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src/CMakeFiles/Serialization.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ builds/schmeckles-all-files/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/src
+# Build directory: /home/ubuntu/forknote-linux/cryptonote-generator/generated_files/schmeckles/build/release/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
